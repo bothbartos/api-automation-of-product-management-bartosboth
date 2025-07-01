ae18bc99bdc7dc385486a51308f3c151fb4ea29d
bartosboth <bothbartos@gmail.com>
2025-07-01 10:39:06 +0200

    updated ProductApiClient

--- src/test/java/com/bartosboth/api/clients/ProductApiClient.java
+++ src/test/java/com/bartosboth/api/clients/ProductApiClient.java
@@ -21,41 +21,23 @@ public class ProductApiClient {
                 .get(PRODUCTS_ENDPOINT);
     }
 
-    public Response getProduct(String id) {
+    public Response getProduct(int id) {
         return given()
-                .pathParam("id", id)
                 .when()
-                .get(PRODUCTS_ENDPOINT + "/${id}");
+                .get(PRODUCTS_ENDPOINT + "/{id}", id);
     }
 
-    public Response updateProduct(String id, Product product) {
+    public Response updateProduct(int id, Product product) {
         return given()
-                .pathParam("id", id)
                 .body(product)
                 .when()
-                .put(PRODUCTS_ENDPOINT + "/${id}");
+                .put(PRODUCTS_ENDPOINT + "/{id}", id);
     }
 
-    public Response deleteProduct(String id) {
+    public Response deleteProduct(int id) {
         return given()
-                .pathParam("id", id)
                 .when()
-                .delete(PRODUCTS_ENDPOINT + "/${id}");
+                .delete(PRODUCTS_ENDPOINT + "/{id}", id);
     }
-
-    public Response getProductsByCategory(String category) {
-        return given()
-                .pathParam("category", category)
-                .when()
-                .get(PRODUCTS_ENDPOINT + "/category/${category}");
-    }
-
-    public Response getProductsWithLimit(int limit) {
-        return given()
-                .pathParam("limit", limit)
-                .when()
-                .get(PRODUCTS_ENDPOINT);
-    }
-
 
 }
